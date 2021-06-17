08be1c815a57fff953b49b402c737537e4e44fab
Ryan Flynn <9076629+Ryan95Z@users.noreply.github.com>
2021-06-17 21:20:44 +0100

    Books Endpoint Improvements (#2)
    * updated protobuf for books to have an id instead of using the title
    
    * fixed the makefile to start the go binary file. Added ids to book endpoints

--- books/v1/books_endpoint.go
+++ books/v1/books_endpoint.go
@@ -2,6 +2,8 @@ package v1
 
 import (
 	context "context"
+	"regexp"
+	"strings"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -28,22 +30,23 @@ func (be *BooksEndpoint) ListBooks(ctx context.Context, request *ListBooksReques
 
 // GetBook returns a book with the given title.
 func (be *BooksEndpoint) GetBook(ctx context.Context, request *GetBookRequest) (*GetBookResponse, error) {
-	title := request.Title
+	id := request.Id
 	for _, book := range books {
-		if book.Title == title {
+		if book.Id == id {
 			return &GetBookResponse{
 				Book: book,
 			}, nil
 		}
 	}
 	return &GetBookResponse{},
-		status.Errorf(codes.NotFound, "book not found with title %s", title)
+		status.Errorf(codes.NotFound, "book not found with id %s", id)
 }
 
 // CreateBook creates a new book and adds it to the service.
 func (be *BooksEndpoint) CreateBook(ctx context.Context, request *CreateBookRequest) (*CreateBookResponse, error) {
 	newBook := new(Book)
 	newBook.Title = request.Title
+	newBook.Id = be.generateBookID(request.Title)
 
 	books = append(books, newBook)
 	return &CreateBookResponse{
@@ -51,13 +54,21 @@ func (be *BooksEndpoint) CreateBook(ctx context.Context, request *CreateBookRequ
 	}, nil
 }
 
+func (be *BooksEndpoint) generateBookID(title string) string {
+	// converts a title into a lowered hyphenated string
+	// e.g `Hello World` becomes `hello-world`
+	whiteSpaceRegex, _ := regexp.Compile(`\s+`)
+	title = strings.ToLower(title)
+	return whiteSpaceRegex.ReplaceAllString(title, "-")
+}
+
 // UpdateBook will update an existing book with the given title.
 func (be *BooksEndpoint) UpdateBook(ctx context.Context, request *UpdateBookRequest) (*UpdateBookResponse, error) {
-	title := request.Title
+	id := request.Id
 	updatedBook := request.Book
 
 	for _, book := range books {
-		if book.Title == title {
+		if book.Id == id {
 			book.Title = updatedBook.Title
 			return &UpdateBookResponse{
 				Book: updatedBook,
@@ -66,14 +77,14 @@ func (be *BooksEndpoint) UpdateBook(ctx context.Context, request *UpdateBookRequ
 	}
 
 	return &UpdateBookResponse{},
-		status.Errorf(codes.NotFound, "book not found with title %s", title)
+		status.Errorf(codes.NotFound, "book not found with id %s", id)
 }
 
 // DeleteBook removes a book from the service with a given name.
 func (be *BooksEndpoint) DeleteBook(ctx context.Context, reqest *DeleteBookRequest) (*DeleteBookResponse, error) {
-	title := reqest.Title
+	id := reqest.Id
 	for idx, book := range books {
-		if book.Title == title {
+		if book.Id == id {
 			books = append(books[:idx], books[idx+1:]...)
 			return &DeleteBookResponse{
 				Done: true,
@@ -81,5 +92,5 @@ func (be *BooksEndpoint) DeleteBook(ctx context.Context, reqest *DeleteBookReque
 		}
 	}
 	return &DeleteBookResponse{},
-		status.Errorf(codes.NotFound, "book not found with title %s", title)
+		status.Errorf(codes.NotFound, "book not found with id %s", id)
 }
